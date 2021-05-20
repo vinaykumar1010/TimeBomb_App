5eeaa76576d8a10348da0e6fcc039ee28dd7d1ab
Vinay Yadav <niovinay@gmail.com>
2021-05-20 14:51:30 +0530

    successsfully added bomb vedio

--- app/src/main/java/com/example/timebomb/MainActivity.java
+++ app/src/main/java/com/example/timebomb/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.timebomb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.media.Image;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -12,13 +14,15 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import java.io.File;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final long START_TIME_IN_MILLIS = 0;
-    private TextView aTextViewCountdown ;
+    private TextView aTextViewCountdown;
     private TextView aEditTextMinInput;
     private TextView aEditTextHourInput;
     private Button aButtonStartPause;
@@ -28,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
     private CountDownTimer mCountDownTimer;
     private long aTimeLeftInMillis = START_TIME_IN_MILLIS;
     private long aStartTimeInMillis;
-
+    private long millisInput = 0 ;
     private long aEndTime;
+    private VideoView bombView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +42,28 @@ public class MainActivity extends AppCompatActivity {
         // means we call on create function of (super ) parent class   so that
         // only after all parent class functions execution then our written funactionality start excuting
         setContentView(R.layout.activity_main);
+       // setTime( long  milliseconds = 0 );
 
         aTextViewCountdown = findViewById(R.id.tv_Countdown);
         aButtonStartPause = findViewById(R.id.bt_start);
         aButtonReset = findViewById(R.id.bt_reset);
-       aButtonSet = findViewById(R.id.bt_set);
-       aEditTextMinInput = findViewById(R.id.et_input_min);
+        aButtonSet = findViewById(R.id.bt_set);
+        aEditTextMinInput = findViewById(R.id.et_input_min);
         aEditTextHourInput = findViewById(R.id.et_input_hour);
+        bombView = findViewById(R.id.virtual_view);
 
         aButtonSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                aButtonStartPause.setVisibility(View.VISIBLE );
                 String input = aEditTextMinInput.getText().toString();
-              // String s2 = aEditTextHourInput.getText().toString();
-               // String input = s1.concat(s2);
-                Log.d("ded","input value is :"+ input );
+                Log.d("ded", "input value is :" + input);
                 if (input.length() == 0) {
                     Toast.makeText(MainActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                long millisInput = Long.parseLong(input) * 60000;
+                 millisInput = Long.parseLong(String.valueOf(input)) * 60000;
+                // convert diretly to milisecond
                 if (millisInput == 0) {
                     Toast.makeText(MainActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
                     return;
@@ -65,8 +72,6 @@ public class MainActivity extends AppCompatActivity {
                 aEditTextMinInput.setText("");
             }
         });
-
-
 
         aButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         aButtonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,13 +97,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setTime(long milliseconds) {
-        Log.d("tag","time is set");
+        Log.d("tag", "time is set");
         aStartTimeInMillis = milliseconds;
         resetTimer();
         closeKeyboard();
     }
 
     private void startTimer() {
+
         // constructor obj dega ,  2 fun ki defination b batao
         mCountDownTimer = new CountDownTimer(aTimeLeftInMillis, 1000) {
             @Override
@@ -112,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 aTimerRunning = false;
                 aButtonStartPause.setText("start");
                 aButtonReset.setVisibility(View.INVISIBLE);
+                playBomb();
 
             }
         }.start();
@@ -121,6 +129,15 @@ public class MainActivity extends AppCompatActivity {
 //        aTimerRunning = true;
 //        aButtonStartPause.setText("pause");
 //        aButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    public void playBomb() {
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bomb);
+        Log.d("BOMB", String.valueOf(uri));
+        bombView.setVideoURI(uri);
+        bombView.setVisibility(View.VISIBLE);
+        bombView.start();
+
     }
 
     private void pauseTimer() {
@@ -133,11 +150,11 @@ public class MainActivity extends AppCompatActivity {
         aTimeLeftInMillis = aStartTimeInMillis;
         updateCountDownText();
         updateWatchInterface();
-    //    aTextViewCountdown.setText(00:00:00);
+        //    aTextViewCountdown.setText(00:00:00);
     }
 
     private void updateCountDownText() {
-        int hours =(int) (aTimeLeftInMillis / 1000) / 3660;
+        int hours = (int) (aTimeLeftInMillis / 1000) / 3660;
         int minutes = (int) (aTimeLeftInMillis / 1000 % 3600) / 60;
         int seconds = (int) (aTimeLeftInMillis / 1000) % 60;
         String timeLeftFormatted;
@@ -173,6 +190,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private void closeKeyboard() {
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -180,6 +198,7 @@ public class MainActivity extends AppCompatActivity {
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -194,6 +213,7 @@ public class MainActivity extends AppCompatActivity {
             mCountDownTimer.cancel();
         }
     }
+
     @Override
     protected void onStart() {
         super.onStart();
